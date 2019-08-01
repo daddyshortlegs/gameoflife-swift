76f11e024672398d021080b5d945e929cf11855b
andys <andy.smith@mkodo.com>
2019-08-01 21:12:35 +0100

    Initial Commit

---
+++ GameOfLife/GameOfLife.h
@@ -0,0 +1,19 @@
+//
+//  GameOfLife.h
+//  GameOfLife
+//
+//  Created by Andy Smith on 01/08/2019.
+//  Copyright © 2019 Andy Smith. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for GameOfLife.
+FOUNDATION_EXPORT double GameOfLifeVersionNumber;
+
+//! Project version string for GameOfLife.
+FOUNDATION_EXPORT const unsigned char GameOfLifeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GameOfLife/PublicHeader.h>
+
+
